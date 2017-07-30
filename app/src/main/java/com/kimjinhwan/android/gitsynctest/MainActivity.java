82b8bc5009aa2599bfa66a9b72825ec272073e07
pleasantlife <pleasantlife@gmail.com>
2017-07-31 01:48:57 +0900

    initial commit

---
+++ app/src/main/java/com/kimjinhwan/android/gitsynctest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kimjinhwan.android.gitsynctest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String layout = "레이아웃";
+        String happyMil = "해피밀";
+    }
+}
