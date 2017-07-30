63ee7e30947508e483bcb8975a3a1a7d67893c25
pleasantlife <pleasantlife@gmail.com>
2017-07-31 02:14:59 +0900

    Add String

--- app/src/main/java/com/kimjinhwan/android/gitsynctest/MainActivity.java
+++ app/src/main/java/com/kimjinhwan/android/gitsynctest/MainActivity.java
@@ -12,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
 
         String layout = "레이아웃";
         String happyMil = "해피밀";
+
+        String momstouch = "맘스터치";
+        String lotteRia = "롯데리아";
+        String popeyes = "파파이스";
     }
 }
